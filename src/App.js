81588866a46d58a86145206078823f5ceb88215e
Nguyen Hiep <hiephap1705@gmail.com>
2023-05-23 15:25:53 +0700

    state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import { Nav } from './views/Nav';
-const App= () => {
+import React from 'react'
+import { useState } from 'react';
+const App = () => {
+
+  const [name, setName] = useState('Hiep');
+  const [address, setAddress] = useState('')
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Learning JS (ReactJS)' },
+    { id: 2, title: 'Playing game' },
+    {id: 3, title: 'Doing homework OOP' },
+  ]);
+  const handleClick = () => {
+    if(!address){
+      alert('Please fill missing information ');
+      return; 
+    }
+    let todo = {id: 'abc', title:address}
+    setTodos([...todos,todo]);
+    setAddress('')
+  }
+  const handleChange = (event) => {
+    setAddress(event.target.value);
+  }
+
+  // dung map khong sua du lieu cua bien goc
+
   return (
     <div className="App">
-        <Nav/>
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {name} from {address} </p>
+
+        <div className='todos-container'>
+          {todos && todos.length > 0 && todos.map((item, index) => {
+             return (<li className='todo-child' key={item.id}>{index + 1} - {item.title} </li>)
+             }
+          )}
+        </div>
+        <input type="text" onChange={(event) => handleChange(event)} value={address} />
+
+
+        <button onClick={() => handleClick()} >Click me</button>
       </header>
     </div>
   );
